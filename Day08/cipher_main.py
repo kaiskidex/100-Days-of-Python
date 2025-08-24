3bb7adda27e5dcc07beefea7de8883ab465359df
kaiskidex <166215444+kaiskidex@users.noreply.github.com>
2025-08-25 01:33:52 +0800

    Added Day-08 ipynb blocks 1-2 and made caesar cipher py file

---
+++ Day08/cipher_main.py
@@ -0,0 +1,51 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, direction):
+    cipher_text = ""
+    if direction == "decode":
+        shift_amount *= -1
+
+    for char in original_text:
+        if char in alphabet:
+            position = alphabet.index(char)  # find position
+            new_position = (position + shift_amount) % 26
+            new_letter = alphabet[new_position]  # get letter
+            cipher_text += new_letter  # add to result
+        else:
+            cipher_text += char
+
+
+    print(f"The {direction}d text is: {cipher_text}")
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+
+    restart = input("Continue? (Y or N): ").upper()
+    if restart == "N":
+        should_continue = False
+        print("Bye bye")
